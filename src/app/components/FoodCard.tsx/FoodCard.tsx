a13ee9be58cb778b2a14fb200f668735a3d996af
unknown <giviagivia9@gmail.com>
2024-06-26 19:38:04 +0400

    improved foodcard.tsx and changed png to svg, added like icon as favourite

---
+++ src/app/components/FoodCard.tsx/FoodCard.tsx
@@ -0,0 +1,103 @@
+'use client';
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import Image from 'next/image';
+
+import styles from './fiid-card.module.css';
+import './food-card.css';
+import { FavoriteEnum } from '@/app/common/enums/favorite.enum';
+
+import 'swiper/css'; // Import Swiper styles
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export interface IFoodResto {
+  images: { name: string }[];
+  name: string;
+  address: string;
+  price: string;
+  favorite: FavoriteEnum;
+  // eslint-disable-next-line no-unused-vars
+  onFavoriteChange: (newFavorite: FavoriteEnum) => void;
+}
+
+// const data = [
+//   {
+//     images: [
+//       { name: '/images/food.png' },
+//       { name: '/images/food1.png' },
+//       { name: '/images/food2.png' },
+//       { name: '/images/food3.png' },
+//     ],
+//     name: 'ბურგერ ბარი',
+//     address: 'ვაჯა-ფშაველას 32',
+//     price: '60 ლ',
+//   },
+// ];
+
+export default function FoodCard(props: IFoodResto) {
+  const { images, name, address, price, favorite, onFavoriteChange } = props;
+
+  const [isFavorite, setIsFavorite] = useState(favorite === 'FAVORITED');
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite); // Toggle favorite state
+
+    if (favorite === FavoriteEnum.UNFAVORITE) {
+      onFavoriteChange(FavoriteEnum.NORMAL); // Update to NORMAL if current state is UNFAVORITE
+    } else {
+      const newFavorite = isFavorite
+        ? FavoriteEnum.NORMAL
+        : FavoriteEnum.FAVORITED;
+      onFavoriteChange(newFavorite); // Update to FAVORITED or NORMAL based on current toggle state
+    }
+  };
+
+  return (
+    <div className={`${styles['food-card']} food-card`}>
+      <Swiper
+        spaceBetween={1}
+        loop={true}
+        slidesPerView={1}
+        modules={[Navigation, Pagination]}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        {images.map((el, index) => (
+          <SwiperSlide key={index}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={el.name}
+                alt={el.name}
+                className={styles['fc-img']}
+                width={300}
+                height={200}
+              />
+              <div className={styles.heartIcon} onClick={toggleFavorite}>
+                <Image
+                  src={
+                    favorite === 'NORMAL'
+                      ? '/images/food-imgs/emptyHeart.svg'
+                      : favorite === 'FAVORITED'
+                        ? '/images/food-imgs/filledHeart.svg'
+                        : '/images/food-imgs/x.svg'
+                  }
+                  width={14}
+                  height={12}
+                  alt="Heart Icon"
+                  style={{ marginTop: '2px' }}
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={styles['fc-info']}>
+        <div className={styles['fc-title']}>{name}</div>
+        <p>{address}</p>
+        <div className={styles['fc-prize']}>{price}</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,27 @@
-import SliderWithThumbnails from '@/app/components/slider-with-thumbnails/SliderWithThumbnails';
-import ImageSlider from './components/slider/Slider';
-import SliderCenterMode from '@/app/components/slider-center-mode/SliderCenterMode';
+'use client';
+// import SliderWithThumbnails from '@/app/components/slider-with-thumbnails/SliderWithThumbnails';
+// import ImageSlider from './components/slider/Slider';
+// import SliderCenterMode from '@/app/components/slider-center-mode/SliderCenterMode';
 import CountryCard from './components/CountryCard/CountryCard';
-import FoodCard from './components/Day-favourite/FoodCard';
+import FoodCard from './components/FoodCard.tsx/FoodCard';
 import Resto from './components/Resto/Resto';
 import OfferedCard from './components/OfferedCard/OfferedCard';
-import { FavoriteEnum } from '@/app/common/enums/favorite.enum';
+import { FavoriteEnum } from './common/enums/favorite.enum';
+import { SetStateAction, useState } from 'react';
 
 export default function Home() {
+  const [favoriteState, setFavoriteState] = useState(FavoriteEnum.UNFAVORITE);
+
+  const handleFavoriteChange = (newFavorite: SetStateAction<FavoriteEnum>) => {
+    setFavoriteState(newFavorite);
+  };
   return (
     <>
-      <SliderCenterMode />
+      {/* <SliderCenterMode />
       <SliderWithThumbnails />
-      <ImageSlider />
+      <ImageSlider /> */}
 
-      <CountryCard name={'test'} image={'/images/country-imgs/khinkali.png'} />
+      <CountryCard name={'test'} image={'/images/country-imgs/khingali.svg'} />
       <FoodCard
         images={[
           { name: '/images/food-imgs/food1.png' },
@@ -25,9 +32,10 @@ export default function Home() {
         name={'ბურგერ ბარი'}
         address={'ვაჯა-ფშაველას 32'}
         price={'60 ლ'}
-        favorite={FavoriteEnum.NORMAL}
+        favorite={favoriteState}
+        onFavoriteChange={handleFavoriteChange}
       />
-      <Resto name={'test'} image={'/images/resto-imgs/respublic.png'} />
+      <Resto name={'test'} image={'/images/resto-imgs/dro.svg'} />
       <OfferedCard
         name={'პიცა რუმი'}
         image={'/images/offered-imgs/offeredPizza.png'}
