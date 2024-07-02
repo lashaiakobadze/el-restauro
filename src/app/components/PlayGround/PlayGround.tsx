b047b7d8fb2d3ac446caddd70e135f3f70be49f1
lashaiakobadze <lashaiakobadze98@gmail.com>
2024-07-02 22:23:23 +0400

    add items at playground

--- src/app/components/PlayGround/PlayGround.tsx
+++ src/app/components/PlayGround/PlayGround.tsx
@@ -12,6 +12,11 @@ import { Tab } from '@/app/types';
 import ItemIndicator from '../ui/ItemIndicator/ItemIndicator';
 import Counter from '../ui/Counter/Counter';
 import ContactItem from '../ui/ContactItem/ContactItem';
+import Orders from '../Orders/Orders';
+import CustomerDetails from '../CustomerDetails/CustomerDetails';
+import CustomerInfo from '../customerInfo/CustomerInfo';
+import { ICustomerInfo } from '../customerInfo/ICustomerInfo';
+import Breadcrumb from '../Breadcrumb/Breadcrumb';
 
 interface PlayGroundProps {}
 
@@ -28,6 +33,12 @@ const PlayGround: React.FC<PlayGroundProps> = () => {
     setActiveTab(tabId);
   };
 
+  const [indicator, setIndicator] = useState(false);
+
+  const onSetIndicator = () => {
+    setIndicator(!indicator);
+  };
+
   const [quantity, setQuantity] = useState(1);
 
   const handleIncrement = () => {
@@ -38,6 +49,22 @@ const PlayGround: React.FC<PlayGroundProps> = () => {
   const handleDecrement = () => {
     const newQuantity = quantity > 0 ? quantity - 1 : 0;
     setQuantity(newQuantity);
+  };
+
+  const customerInfo: ICustomerInfo = {
+    lat: 41.7151,
+    lng: 44.8271,
+    address: 'თბილისი, თამარ ჭოველიძის 7 ნომერი',
+    phone: '032 2 123 321',
+    workingHours: [
+      'Monday: 9 AM - 5 PM',
+      'Tuesday: 9 AM - 5 PM',
+      'Wednesday: 9 AM - 5 PM',
+      'Thursday: 9 AM - 5 PM',
+      'Friday: 9 AM - 5 PM',
+      'Saturday: 10 AM - 2 PM',
+      'Sunday: Closed',
+    ],
   };
 
   return (
@@ -79,6 +106,7 @@ const PlayGround: React.FC<PlayGroundProps> = () => {
       <h2>Buttons</h2>
       <div className={styles.items}>
         <ActionButton label="Primary" onClick={() => console.log('Primary')} />
+
         <Button
           text={'ყიდვა'}
           color="#23447C"
@@ -88,13 +116,20 @@ const PlayGround: React.FC<PlayGroundProps> = () => {
 
       <h2>UI elements</h2>
       <div className={styles.items}>
+        <Breadcrumb />
+
         <ContactItem
           icon={'/images/contacts/fb.svg'}
           link="https://www.instagram.com"
         />
 
-        <ItemIndicator isActive={true} />
-        <ItemIndicator isActive={false} />
+        <div onClick={() => onSetIndicator()}>
+          <ItemIndicator isActive={indicator} />
+        </div>
+
+        <div onClick={() => onSetIndicator()}>
+          <ItemIndicator isActive={!indicator} />
+        </div>
 
         <Counter
           quantity={quantity}
@@ -108,6 +143,16 @@ const PlayGround: React.FC<PlayGroundProps> = () => {
           onDecrement={() => handleDecrement()}
           hasBorder={false}
         />
+      </div>
+
+      <h2>Custom components</h2>
+      <div className={styles.items}>
+        <div style={{ width: '900px' }}>
+          <CustomerDetails />
+        </div>
+        <Orders />
+
+        <CustomerInfo customerInfo={customerInfo} />
       </div>
     </div>
   );
